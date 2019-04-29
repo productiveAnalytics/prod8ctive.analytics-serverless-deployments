6b8ca937b5164663db8db369f962499d37273d8b
Chawathe, Lalit <lalit.chawathe@changehealthcare.com>
2019-04-29 00:36:53 -0500

    Event processing for DynamoDB streams

--- handler.js
+++ handler.js
@@ -1,15 +1,31 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.hello = async (event, context, callback) => {
   console.log(`Successfully received ${event.Records.length} records.`);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
-
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: 'Go Serverless v1.0! Your function executed successfully!',
+  //     input: event,
+  //   }, null, 2),
+  // };
+  
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  //console.log('Received event:', JSON.stringify(event, null, 2));
+  event.Records.forEach((record) => {
+    console.log('Stream record: ', JSON.stringify(record, null, 2));
+    console.log(record.eventName);
+    console.log(record.eventID);
+    if ('INSERT' == record.eventName) {
+      console.log('INSERT: DynamoDB NEW Record: %j', record.dynamodb.NewImage);
+    } else if ('MODIFY' == record.eventName) {
+      console.log('UPDATE: DynamoDB OLD Record: %j', record.dynamodb.OldImage);
+      console.log('UPDATE: DynamoDB NEW Record: %j', record.dynamodb.NewImage);
+    } else if ('REMOVE' == record.eventName) {
+      console.log('DELETE: DynamoDB OLD Record: %j', record.dynamodb.OldImage);
+    }
+  });
+
+  callback(null, `Successfully processed ${event.Records.length} records.`);
 };
